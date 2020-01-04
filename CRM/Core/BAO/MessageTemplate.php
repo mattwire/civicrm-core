90240622f840ca228cbcc3d1b4200a08843f8abd
Matthew Wire <mjw@mjwconsult.co.uk>
2020-01-04 16:57:21 +0000

    Add MessageTemplate.clone API

--- CRM/Core/BAO/MessageTemplate.php
+++ CRM/Core/BAO/MessageTemplate.php
@@ -620,4 +620,25 @@ class CRM_Core_BAO_MessageTemplate extends CRM_Core_DAO_MessageTemplate {
     ])->fetchMap('name', 'id');
   }
 
+  /**
+   * Make a copy.
+   *
+   * @param int $id The entity id to copy.
+   * @param array $params
+   * @return CRM_Core_DAO
+   */
+  public static function copy($id, $params = []) {
+    $fieldsFix = [
+      'suffix' => [
+        'msg_title' => ' - ' . ts('Copy'),
+      ],
+      'replace' => $params,
+    ];
+    $copy = CRM_Core_DAO::copyGeneric('CRM_Core_DAO_MessageTemplate', ['id' => $id], NULL, $fieldsFix);
+    $copy->save();
+    CRM_Utils_Hook::copy('MessageTemplate', $copy);
+
+    return $copy;
+  }
+
 }
